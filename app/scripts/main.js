d0a3419faf08ac96fda38f37fd6ba6a1eb24247e
Liam McCabe <liampmccabe@gmail.com>
2015-05-25 18:39:01 +0100

    v2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,14 @@
 'use strict';
 
-/* jshint devel:true */
-
 // Matter.js - http://brm.io/matter-js/
+
+setTimeout(function() {
+  document.getElementById('signup').className += 'visible';
+}, 3000);
+
+setTimeout(function() {
+  document.getElementById('social').className += 'visible';
+}, 5000);
 
 // Matter module aliases
 var Engine = Matter.Engine,
@@ -22,8 +28,8 @@ var Engine = Matter.Engine,
 
     // MatterTools aliases
     if (window.MatterTools) {
-        var Gui = MatterTools.Gui,
-            Inspector = MatterTools.Inspector;
+      var Gui = MatterTools.Gui,
+          Inspector = MatterTools.Inspector;
     }
 
     var Demo = {};
@@ -37,8 +43,8 @@ var Engine = Matter.Engine,
         _useInspector = window.location.hash.indexOf('-inspect') !== -1,
         _isMobile = /(ipad|iphone|ipod|android)/gi.test(navigator.userAgent);
     
-    // initialise the demo
 
+    // initialise the demo
     Demo.init = function() {
 
         var container = document.getElementById('cells');
@@ -54,17 +60,15 @@ var Engine = Matter.Engine,
         // NOTE: this is actually Matter.Engine.create(), see the aliases at top of this file
         _engine = Engine.create(container, options);
 
-        // add a mouse controlled constraint
-        //_mouseConstraint = MouseConstraint.create(_engine);
-        //World.add(_engine.world, _mouseConstraint);
-
         // run the engine
         Engine.run(_engine);
 
         // set up a scene with bodies
-        Demo['gravity']();
+        Demo['cells']();
 
     };
+
+
 
     // call init when the page has loaded fully
     if (window.addEventListener) {
@@ -73,7 +77,10 @@ var Engine = Matter.Engine,
       window.attachEvent('load', Demo.init);
     }
 
-    Demo.gravity = function() {
+
+
+    Demo.cells = function() {
+
       var _world = _engine.world;
       
       Demo.reset();
@@ -85,14 +92,8 @@ var Engine = Matter.Engine,
       renderOptions.wireframes = false;
       renderOptions.width = window.innerWidth*2;
       renderOptions.height = window.innerHeight*2;
-      //renderOptions.showAxes = true;
-      //renderOptions.showCollisions = true;
-      //renderOptions.showBounds = true;
-      //renderOptions.showIds = true;
-      //renderOptions.hasBounds = true;
       renderOptions.showAngleIndicator = false;
       renderOptions.background = 'rgba(255,255,255,0)';
-      //renderOptions.wireframeBackground = 'rgba(0,0,0,0)';
 
       _engine.render.canvas.width = window.innerWidth*2;
       _engine.render.canvas.height = window.innerHeight*2;
@@ -102,42 +103,111 @@ var Engine = Matter.Engine,
 
       _engine.render.context.scale(2,2);
 
-      //World.add(_world, circle);
+      var offset = 0;
+      World.add(_world, [
+
+          Bodies.rectangle(0, 0, 10, window.innerHeight*2, { isStatic: true, render: { fillStyle: 'rgba(255,255,255,0)', strokeStyle: 'rgba(255,255,255,0)'} }),
+
+          Bodies.rectangle(window.innerWidth, 0, 10, window.innerHeight*2, { isStatic: true, render: { fillStyle: 'rgba(255,255,255,0)', strokeStyle: 'rgba(255,255,255,0)'} }),
+
+          Bodies.rectangle(0, 0, window.innerWidth*2, 10, { isStatic: true, render: { fillStyle: 'rgba(255,255,255,0)', strokeStyle: 'rgba(255,255,255,0)'} }),
+
+          Bodies.rectangle(0, window.innerHeight, window.innerWidth*2-10, 10, { isStatic: true, render: { fillStyle: 'rgba(255,255,255,0)', strokeStyle: 'rgba(255,255,255,0)'} })
+      ]);
+
 
       var cellCount = 0;
-      var cells = [];
-      var nucleus = [];
-      var overlayOpacity = 0;
+
+      var cell = function(xx,yy,radius) {
+
+        //Some cell variables
+        var opacity = Common.random(3,80)/100;
+        opacity = opacity.toFixed(2);
+
+
+        //The main cell composite
+        var cellComposite = Composite.create({ label: 'Cell' });
+
+        //Membrane body - outer circle
+        var membrane = Bodies.circle(xx, yy, radius, {
+          label: 'Membrane',
+          friction: 0.00001, 
+          restitution: 0.0001, 
+          density: 0.1,
+          render: {
+            fillStyle: 'rgba(255,255,255,0)',
+            fillStyleTo: 'rgba(255,255,255,0)',
+            strokeStyle: 'rgba(255,255,255,0)',
+            strokeStyleTo: 'rgba(255,255,255,'+opacity+')',
+            lineWidth: 2
+          }
+        });
+
+        //Nucleus body - inner circle
+        var nucleus = Bodies.circle(xx, yy, radius/5, {
+          label: 'Nucleus',
+          friction: 0, 
+          restitution: 0, 
+          density: 0.1,
+          render: {
+            fillStyle: 'rgba(255,255,255,0)',
+            fillStyleTo: 'rgba(255,255,255,'+opacity+')',
+            strokeStyle: 'rgba(255,255,255,0)',
+            lineWidth: 0
+          }
+        });
+
+        //Add a bodies to the composite
+        Composite.addBody(cellComposite, membrane);
+        Composite.addBody(cellComposite, nucleus);
+
+        //Reset nucleus physics
+        nucleus.slop = 0;
+        nucleus.friction = 0;
+        nucleus.density = 0;
+        nucleus.inertia = 0;
+
+        if(Common.random(1,600) > 590) {
+          cellComposite.cancer = true;
+        }
+
+        cellComposite.bodies[0].parentId = cellComposite.id;
+
+        return cellComposite;
+      }
+
+
+
+      //Email input listener
+      document.getElementById('mce-EMAIL').addEventListener('keydown', function() {
+        inputKeyDown = true;
+      });
+
+
+
 
       _sceneEvents.push(
 
         // an example of using collisionStart event on an engine
         Events.on(_engine, 'collisionStart', function(event) {
-            var pairs = event.pairs;
 
-            // change object colours to show those starting a collision
-            for (var i = 0; i < pairs.length; i++) {
+          var pairs = event.pairs;
 
-                var pair = pairs[i];
+          var cells = Composite.allComposites(_engine.world);
 
-                if(overlayOpacity < 1) overlayOpacity += 0.0005;
-                overlayOpacity = parseFloat(overlayOpacity.toFixed(5));
+          for (var i = 0; i < cells.length; i++) {
 
-                if(pair.bodyA.cancer) {
-                  pair.bodyB.cancer = true;
-                  pair.bodyB.render.fillStyle = pair.bodyA.render.fillStyle;
-                  pair.bodyB.render.strokeStyle = pair.bodyA.render.strokeStyle;
-                  document.getElementsByClassName('overlay')[0].style.opacity = overlayOpacity;
-                } else if (pair.bodyB.cancer) {
-                  pair.bodyA.cancer = true;
-                  pair.bodyA.render.fillStyle = pair.bodyB.render.fillStyle;
-                  pair.bodyA.render.strokeStyle = pair.bodyB.render.strokeStyle;
-                  document.getElementsByClassName('overlay')[0].style.opacity = overlayOpacity;
-                }
-            }
+            var cell = cells[i];
+            var membrane = cell.bodies[0];
+
+
+          }
+
         })
 
       );
+
+
 
       _sceneEvents.push(
 
@@ -146,46 +216,17 @@ var Engine = Matter.Engine,
 
           var engine = event.source;
 
-          if (event.timestamp % 10 < 50) {
+          if (event.timestamp % 10 < 20) {
 
             flow(engine);
 
-            if(cellCount < 350) {
-              ++cellCount;
+            if(++cellCount < 200) {
 
-              cells[cellCount] = {};
-              cells[cellCount].opacity = 0;
-              cells[cellCount].maxOpacity = Common.random(5,15)/100;
-              cells[cellCount].radius = 0;
-              cells[cellCount].maxRadius = Common.random(4, 30);
+              var x = Common.random(50, window.innerWidth-50);
+              var y = Common.random(50, window.innerHeight-50);
+              var radius = Common.random(3, 30);
 
-              var x = Common.random(0, window.innerWidth);
-              var y = Common.random(0, window.innerHeight);
-
-              nucleus[cellCount] = {};
-
-              World.add(_world, Bodies.circle(x, y, cells[cellCount].maxRadius, { 
-                friction: 0.00001, 
-                restitution: 0.0001, 
-                density: 0.1,
-                render: {
-                  fillStyle: 'rgba(255,255,255,0)',
-                  strokeStyle: 'rgba(255,255,255,0)',
-                  lineWidth: 2
-                }
-              }));
-
-              World.add(_world, Bodies.circle(x, y, cells[cellCount].maxRadius/10, {
-                nucleus: true,
-                friction: 0, 
-                restitution: 0,
-                density: 0,
-                render: {
-                  fillStyle: 'rgba(255,255,255,'+cells[cellCount].maxOpacity+')',
-                  strokeStyle: 'rgba(255,255,255,0)',
-                  lineWidth: 0
-                }
-              }));
+              World.add(_world, cell(x, y, radius));
 
             }
             
@@ -196,48 +237,74 @@ var Engine = Matter.Engine,
       );
 
       var flow = function(engine) {
-        var bodies = Composite.allBodies(engine.world);
 
-          for (var i = 0; i < bodies.length; i++) {
-              var body = bodies[i];
+        var cells = Composite.allComposites(engine.world);
 
-              if(typeof cells[i] !== 'undefined') {
+        for (var i = 0; i < cells.length; i++) {
 
-                if(!body.nucleus) {
-                  if(i % 50 == 0) {
-                    if(cells[i].opacity < cells[i].maxOpacity) {
-                      cells[i].opacity += 0.001;
-                      body.cancer = true;
-                      body.render.fillStyle = 'rgba(255,0,0,'+(cells[i].opacity)+')';
-                      body.render.strokeStyle = 'rgba(255,0,0,'+(cells[i].opacity+0.2)+')';
-                    }
-                  } else {
-                    if(cells[i].opacity < cells[i].maxOpacity) {
-                      cells[i].opacity += 0.001;
-                      body.render.strokeStyle = 'rgba(255,255,255,'+cells[i].opacity+')';
-                    }
-                  }
-                }
+          var cell = cells[i];
+          var nucleus = cell.bodies[1];
+          var membrane = cell.bodies[0];
+          var forceMagnitude;
 
-                if(body.nucleus && typeof nucleus[i] !== 'undefined') {
-                  body.position.x = bodies[i-1].position.x;
-                  body.position.y = bodies[i-1].position.y;
-                }
-
-              }
-
-              if (!body.isStatic && Common.random(0,100) > 33) {
-
-                var forceMagnitude = 0.00004 * body.mass;
-
-                Body.applyForce(body, { x: 0, y: 0 }, { 
-                    x: (forceMagnitude + Common.random(-0.2,0.2) * forceMagnitude) * Common.choose([1, -1]), 
-                    y: (forceMagnitude + Common.random(-0.2,0.2) * forceMagnitude) * Common.choose([1, -1])
-                });
-
-              }
+          if(cell.cancer) {
+            nucleus.render.fillStyle = tween(nucleus.render.fillStyle, 'rgba(255,0,0,0.8)');
+            membrane.render.fillStyle = tween(membrane.render.fillStyle, 'rgba(255,0,0,0)');
+            membrane.render.strokeStyle = tween(membrane.render.strokeStyle, 'rgba(255,0,0,0.8)');
+          } else {
+            nucleus.render.fillStyle = tween(nucleus.render.fillStyle, nucleus.render.fillStyleTo);
+            membrane.render.fillStyle = tween(membrane.render.fillStyle, membrane.render.fillStyleTo);
+            membrane.render.strokeStyle = tween(membrane.render.strokeStyle, membrane.render.strokeStyleTo);
           }
-        };
+
+          nucleus.position.x = membrane.position.x;
+          nucleus.position.y = membrane.position.y;
+
+          if(cell.cancer)
+            forceMagnitude = 0.005 * cell.bodies[1].mass;
+          else
+            forceMagnitude = 0.0002 * cell.bodies[1].mass;
+
+          Body.applyForce(cell.bodies[0], { x: 0, y: 0 }, { 
+              x: (forceMagnitude + Common.random(-1,1) * forceMagnitude) * Common.choose([1, -1]), 
+              y: (forceMagnitude + Common.random(-1,1) * forceMagnitude) * Common.choose([1, -1])
+          });
+
+        }
+      };
+
+      var tween = function(from, to) {
+
+        var prevColor = {}, 
+            nextColor = {};
+
+        prevColor.r = parseInt(from.split(',')[0].split('(')[1]);
+        prevColor.g = parseInt(from.split(',')[1]);
+        prevColor.b = parseInt(from.split(',')[2]);
+        prevColor.a = parseFloat(from.split(',')[3]);
+
+        nextColor.r = parseInt(to.split(',')[0].split('(')[1]);
+        nextColor.g = parseInt(to.split(',')[1]);
+        nextColor.b = parseInt(to.split(',')[2]);
+        nextColor.a = parseFloat(to.split(',')[3]);
+
+        if(prevColor.r != nextColor.r)
+          prevColor.r = (prevColor.r < nextColor.r) ? prevColor.r += 10 : prevColor.r -= 10;
+
+        if(prevColor.g != nextColor.g)
+          prevColor.g = (prevColor.g < nextColor.g) ? prevColor.g += 10 : prevColor.g -= 10;
+        
+        if(prevColor.b != nextColor.b)
+          prevColor.b = (prevColor.b < nextColor.b) ? prevColor.b += 10 : prevColor.b -= 10;
+
+        if(prevColor.a != nextColor.a)
+          prevColor.a = (prevColor.a < nextColor.a) ? prevColor.a += 0.01 : prevColor.a -= 0.01;
+
+        from = 'rgba('+prevColor.r+','+prevColor.g+','+prevColor.b+','+prevColor.a+')';
+
+        return from;
+
+      };
 
     };
 
@@ -256,11 +323,6 @@ var Engine = Matter.Engine,
       for (var i = 0; i < _sceneEvents.length; i++)
           Events.off(_engine, _sceneEvents[i]);
 
-      /*if (_mouseConstraint.events) {
-          for (i = 0; i < _sceneEvents.length; i++)
-              Events.off(_mouseConstraint, _sceneEvents[i]);
-      }*/
-
       if (_world.events) {
           for (i = 0; i < _sceneEvents.length; i++)
               Events.off(_world, _sceneEvents[i]);
@@ -274,36 +336,10 @@ var Engine = Matter.Engine,
       // reset random seed
       Common._seed = 0;
 
-      // reset mouse offset and scale (only required for Demo.views)
-      //Mouse.setScale(_mouseConstraint.mouse, { x: 1, y: 1 });
-      //Mouse.setOffset(_mouseConstraint.mouse, { x: 0, y: 0 });
-
       _engine.enableSleeping = false;
       _engine.world.gravity.y = 1;
       _engine.world.gravity.x = 0;
       _engine.timing.timeScale = 1;
-
-      /*World.add(_world, [
-          Bodies.rectangle(640, 330, 360, 200, { 
-          isStatic: true,
-          friction: 0.00001, 
-          restitution: 0.5, 
-          density: 0.001,
-          render: {
-             fillStyle: 'rgba(255,255,255,0)',
-             strokeStyle: 'rgba(255,255,255,0)',
-          }})
-      ]);*/
-
-      /*var offset = 25;
-      World.add(_world, [
-          Bodies.rectangle(400, -offset, 800.5 + 2 * offset, 50.5, { isStatic: true }),
-          Bodies.rectangle(400, 600 + offset, 800.5 + 2 * offset, 50.5, { isStatic: true }),
-          Bodies.rectangle(800 + offset, 300, 50.5, 600.5 + 2 * offset, { isStatic: true }),
-          Bodies.rectangle(-offset, 300, 50.5, 600.5 + 2 * offset, { isStatic: true })
-      ]);*/
-
-      //World.add(_world, _mouseConstraint);
       
       var renderOptions = _engine.render.options;
       renderOptions.wireframes = true;
@@ -321,5 +357,5 @@ var Engine = Matter.Engine,
       renderOptions.background = '#fff';
 
       if (_isMobile)
-          renderOptions.showDebug = true;
+        renderOptions.showDebug = true;
     };
